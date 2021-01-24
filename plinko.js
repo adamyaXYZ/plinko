8c38ab8596d534fbeb90fb807028fc09175cd3b1
adamyaXYZ <74367616+adamyaXYZ@users.noreply.github.com>
2021-01-24 15:45:36 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,18 @@
+class plinko {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.body = Bodies.circle(x,y,10,options)
+        World.add(world,this.body)
+        
+    }
+    display(){
+        var pos = this.body.position
+        push()
+            translate(pos.x,pos.y)
+            ellipseMode(RADIUS)
+            ellipse(0,0,10,10)
+        pop()
+    }
+} 
